7f8971d6720e2172a8101321e883cff02783339b
MrAndiGamesDev <andielmendez4@gmail.com>
2025-11-11 10:17:11 -0500

    Fixed

---
+++ src/cogs/say.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class Say(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="say")
+    @commands.has_permissions(administrator=True)
+    async def say(self, ctx, channel: discord.TextChannel, *, message: str):
+        """Make the bot say something in the specified channel."""
+        await channel.send(message)
+
+async def setup(bot):
+    await bot.add_cog(Say(bot))
